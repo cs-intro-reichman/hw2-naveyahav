dab50edccba685e807e03cff4bb2258ab682a196
naveyahav <nave.yahav@post.runi.ac.il>
2024-11-14 12:44:32 +0200

    fourth

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,20 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
-	}
+		int sequence = Integer.parseInt(args[0]);
+		double sum =1.0;
+		int counter = 1;
+		for (int i = 0; i < sequence; i++) {
+			counter+=2;
+			double x = 1.0/counter;
+			counter+=2;
+			double y = 1.0/counter;
+			sum += (-x+y);
+		}
+		System.out.println("pi according to Java: " + sum*4);
+		System.out.println("pi, approximated:     "+ Math.PI);
+
+
+	
+}
 }
