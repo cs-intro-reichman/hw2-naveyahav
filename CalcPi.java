3c34bf880f9efe4c1a05f061cdb2a45973153518
naveyahav <nave.yahav@post.runi.ac.il>
2024-11-14 14:26:25 +0200

    fourth

--- CalcPi.java
+++ CalcPi.java
@@ -2,14 +2,16 @@
 public class CalcPi {
 	public static void main(String [] args) { 
 		int sequence = Integer.parseInt(args[0]);
-		double sum =1.0;
+		double sum = 1.0;
 		int counter = 1;
-		for (int i = 0; i < sequence; i++) {
+		int sign = -1;
+		for (int i = 0; i < sequence-1 ; i++) {
 			counter+=2;
+			System.out.println(counter);
 			double x = 1.0/counter;
-			counter+=2;
-			double y = 1.0/counter;
-			sum += (-x+y);
+			System.out.println(x);
+			sum += (x*sign);
+			sign*=-1;
 		}
 		System.out.println("pi according to Java: " + Math.PI);
 		System.out.println("approximated:     "+ sum*4);
